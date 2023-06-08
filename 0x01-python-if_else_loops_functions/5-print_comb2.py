5fb4fa123a2861d8916163d69ce75aaf3b1ebbba
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-06-08 00:15:22 -0700

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for number in range(0, 100):
+for number in range(0, 99):
     print("{:02}".format(number), end=", ")
 
-print("{:02}".format(number));
+print("{:02}".format(number+1));
