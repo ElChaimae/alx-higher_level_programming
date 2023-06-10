a05714c8523ae95d57375c75c88f86e4115b411d
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-06-09 18:33:37 -0700

    Fix errors

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+import hidden_4
+
+
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
 if __name__ == "__main__":
-    import hidden_4
-    temp = dir(hidden_4)
-    for i in range(len(temp)):
-        for j in range(len(temp[i])):
-            if (temp[i][j] == '_' and temp[i][j+1] == '_'):
-                break
-        else:
-            print(temp[i])
-            break
+    discovr()
