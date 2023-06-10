e47c0fd79ca054387b1ac105e8d0f3c35819c91d
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-06-09 18:21:42 -0700

    Fix errors

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-import hidden_4
-
-i = 0
-
-if __name__ == '__main__':
-    l = dir(hidden_4)
-    thelist = sorted(l)
-    while i < len(thelist):
-        if thelist[i][0] != '_':
-            print(thelist[i])
-    i += 1
+if __name__ == "__main__":
+    import hidden_4
+    temp = dir(hidden_4)
+    for i in range(len(temp)):
+        for j in range(len(temp[i])):
+            if (temp[i][j] == '_' and temp[i][j+1] == '_'):
+                break
+        else:
+            print(temp[i])
+            break
