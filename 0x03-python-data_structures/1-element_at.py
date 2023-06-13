c119d3a21d9e0aaf415128b5f80b0672dea8ff68
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-06-12 17:08:12 -0700

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,8 @@
+#!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-
     max = len(my_list) - 1
 
-    elif idx > max:
+    if idx < 0 or idx > max:
         return None
-
     else:
         return my_list[idx]
