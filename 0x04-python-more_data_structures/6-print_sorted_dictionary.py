b710b2399312df98cf7eb6ba5fbb4e0977e72e69
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-06-14 15:10:19 -0700

    Task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_d = sorted(a_dictionary.keys())
+    for key in sorted_d:
+        print(key + ": " + str(a_dictionary[key]))
