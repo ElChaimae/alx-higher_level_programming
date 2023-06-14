fa780fdfbe3a37fe6c67598492be3009fa1b599e
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-06-13 17:46:49 -0700

    Task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+output = ""
+
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        print(chr(i), end='')
+        output += chr(i)
     else:
-        print(chr(i).upper(), end='')
+        output += chr(i).upper()
+
+print(output, end='')
