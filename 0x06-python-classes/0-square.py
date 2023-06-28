6b49f73fd0dd6ae1ee967924e80065e5dc444401
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-06-28 15:06:59 -0700

    Add documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Represents a square.
+
+    This class provides a basic template for creating square objects.
+    """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Represents a square with a specific size.
+
+    This class provides a way to create and manipulate square objects.
+
+    Attributes:
+    __size (int): The size of the square.
+
+    """
     def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+        size (int): The size of the square. Must be a positive integer.
+
+        """
         self.__size = size
