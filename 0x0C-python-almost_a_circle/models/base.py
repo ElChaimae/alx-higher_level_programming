56677a86bfe29a0056f98f10fce104334f6d7868
Chaimae <chaimae_el_maataoui@gmail.com>
2023-09-18 04:54:57 -0700

    Task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/user/bin/python3
+
+
+class Base:
+    """ This class is the “base” of all other classes in this project """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Class Constructor
+        Args:
+            id (int): instance identifier
+        """
+        if id is not None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
