2223e22271f276c1398210b443fa46396326e4de
Chaimae <chaimae_el_maataoui@gmail.com>
2023-09-18 17:44:15 -0700

    Task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,9 +27,6 @@ class Base:
         """
         It returns the JSON string representation of list_dictionaries
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        if (type(list_dictionaries) != list or not
-                all(type(i) == dict for i in list_dictionaries)):
-            raise TypeError("list_dictionaries must be a list of dictionaries")
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"  
         return json.dumps(list_dictionaries)
