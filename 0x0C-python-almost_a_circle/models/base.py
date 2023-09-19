ec3c872bdde9203abffaef468b7ecc932b730564
Chaimae <chaimae_el_maataoui@gmail.com>
2023-09-18 17:34:06 -0700

    Task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 Defines a Base Class
 """
 
+import json
+
 
 class Base:
     """ This class is the “base” of all other classes in this project """
@@ -19,3 +21,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        It returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
